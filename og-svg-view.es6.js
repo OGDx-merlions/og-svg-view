2a3e0bcc86b40d2c700796977ebe9171629e1a19
De Silva, Sunna Deniyage Chamara Gihan (Gihan Chamara) <gihan.chamara@bhge.com>
2018-08-06 15:38:31 +0800

    add event listners

--- og-svg-view.es6.js
+++ og-svg-view.es6.js
@@ -9,12 +9,26 @@ Polymer({
      svgSrc:{
        type:String,
        observer:'_srcSVGUpdated'
+     },
+     /**
+      * Elements to bind with on click action
+      * eg : {id:'pad-1-well',listner:(()=>{alert('clicked')}),class:'active-el'}
+      * id:elements id in SVG
+      * listner: call back method to be triggered on click event
+      * class : style class to be applied on element
+      * 
+      * @property activeElements
+      */
+     activeElements:{
+        type:Array,
+        value:[{id:'pad-1-well',listner:(()=>{alert('clicked')}),class:'active-el'}],
+        observer:'_bindSVGElements'
      }
     },
     ready:function(){
         let mouseMoveHander = this._elementDrag.bind(this);
         this.$.svg.addEventListener('mousedown',(e)=>{this._dragMouseDown(e,mouseMoveHander,this)});
-        window.addEventListener('mouseup',(e)=>{this._closeDragElement(e,mouseMoveHander,this)});
+        window.addEventListener('mouseup',(e)=>{this._closeDragElement(e,mouseMoveHander,this)});        
     },
     load:function(){
 
@@ -51,6 +65,40 @@ Polymer({
       _svgAJAXResponse:function(event,req){
        let response = event.detail.response;
        this.$.svg.innerHTML = response;
+       this._bindSVGElements();
+      },
+      _bindSVGElements:function(){
+        this.activeElements.forEach(r => {            
+            let elList =this.$.svg.querySelectorAll('#'+r.id);
+            if(elList.length==0){
+                console.log('Error :cannot find elements for the id '+r.id);
+                return;
+            }
+            if(elList && elList.length>1){
+                console.log('Error : multiple elements with same id for the id '+r.id);
+                return;
+            }
+            let el = elList[0];
+            el.addEventListener('click',r.listner);
+            el.classList.add(r.class);
+        });
+      },
+      showHideElement:function(id,show){
+        let elList =this.$.svg.querySelectorAll('#'+id);
+        if(elList.length==0){
+            console.log('Error :cannot find elements for the id '+id);
+            return;
+        }
+        if(elList && elList.length>1){
+            console.log('Error : multiple elements with same id for the id '+id);
+            return;
+        }
+        let el = elList[0]; 
+        if(show) {
+            el.classList.remove('hidden');
+        }else{
+            el.classList.add('hidden');
+        }          
       }
 
   });
