0abe6c6459b6972487ea5da7da77aea59213109c
De Silva, Sunna Deniyage Chamara Gihan (Gihan Chamara) <gihan.chamara@bhge.com>
2018-08-07 12:39:24 +0800

    update watch-list view

---
+++ watch-list.es6.js
@@ -0,0 +1,17 @@
+Polymer({
+    is: 'watch-list',
+    properties: {
+      /**
+      * Src path of SVG
+      *
+      * @property svgSrc
+      */     
+     title:{
+       type:String       
+     }
+     
+    },
+    ready:function(){
+            
+    }
+  });
